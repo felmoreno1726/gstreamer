a65cd32ecd56c0ddd4ad0f034482acfcec8b9685
Felipe Moreno <felipemoreno1626@gmail.com>
2024-01-03 04:19:10 -0500

    tutorial 2 solution with exercise

--- subprojects/gst-docs/examples/tutorials/basic-tutorial-2.c
+++ subprojects/gst-docs/examples/tutorials/basic-tutorial-2.c
@@ -7,7 +7,7 @@
 int
 tutorial_main (int argc, char *argv[])
 {
-  GstElement *pipeline, *source, *sink;
+  GstElement *pipeline, *source, *sink, *filter;
   GstBus *bus;
   GstMessage *msg;
   GstStateChangeReturn ret;
@@ -17,19 +17,20 @@ tutorial_main (int argc, char *argv[])
 
   /* Create the elements */
   source = gst_element_factory_make ("videotestsrc", "source");
+  filter = gst_element_factory_make("vertigotv", "filter");
   sink = gst_element_factory_make ("autovideosink", "sink");
 
   /* Create the empty pipeline */
   pipeline = gst_pipeline_new ("test-pipeline");
 
-  if (!pipeline || !source || !sink) {
+  if (!pipeline || !filter || !source || !sink) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
   }
 
   /* Build the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
-  if (gst_element_link (source, sink) != TRUE) {
+  gst_bin_add_many (GST_BIN (pipeline), source, filter, sink, NULL);
+  if (gst_element_link (source, filter) != TRUE) || (gst_element_link (filter, sink) != TRUE)) {
     g_printerr ("Elements could not be linked.\n");
     gst_object_unref (pipeline);
     return -1;
